baeaa7a2622cacc4fa96da31a0f552b5d934eddb
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-08-20 17:24:20 +0200

    improved background image

--- Scripts/ScriptProcessors/newRealm/Interface.js
+++ Scripts/ScriptProcessors/newRealm/Interface.js
@@ -3,7 +3,7 @@ Content.makeFrontInterface(900, 600);
 include("ZoomHandler.js");
 
 const var background = Content.getComponent("background");
-background.loadImage("{PROJECT_FOLDER}background.jpg", "background");
+background.loadImage("{PROJECT_FOLDER}background.png", "background");
 background.setPaintRoutine(function(g)
 {
 	var a = [0, 0, 900, 600];
