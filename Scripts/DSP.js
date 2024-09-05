69d72c6182eae26cab5dfa4f6e87bbdfd886cf4b
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-09-05 23:30:28 +0200

    fixed gui on gain control

--- Scripts/DSP.js
+++ Scripts/DSP.js
@@ -1,19 +1,20 @@
 namespace DSP {
-
-// ---------------- DSP ---------------- //
 const var sampleMaps = Sampler.getSampleMapList();
 
 const var attackSampler = Synth.getSampler("attackSampler");
-inline function onAttackSelectorControl(component, value)
-{
+inline function onAttackSelectorControl(component, value) {
 	attackSampler.loadSampleMap(sampleMaps[value - 1]);
 };
 Content.getComponent("attackSelector").setControlCallback(onAttackSelectorControl);
 
 const var sustainSampler = Synth.getSampler("sustainSampler");
-inline function onSustainSelectorControl(component, value)
-{
+inline function onSustainSelectorControl(component, value) {
 	sustainSampler.loadSampleMap(sampleMaps[value - 1 + 5]);
 };
 Content.getComponent("sustainSelector").setControlCallback(onSustainSelectorControl);
+
+inline function onAttackGainControl(component, value) {
+        attackGainPanel.repaint();
+    };
 } 
+
--- Scripts/LookAndFeel.js
+++ Scripts/LookAndFeel.js
@@ -1,124 +1,120 @@
-
 namespace LookAndFeel {
 
-const var background = Content.getComponent("background");
-background.loadImage("{PROJECT_FOLDER}background.png", "background");
-background.setPaintRoutine(function(g)
-{
-	var a = [0, 0, 900, 600];
-	g.drawImage("background", a, 0, 0);
-});
+    const var background = Content.getComponent("background");
+    background.loadImage("{PROJECT_FOLDER}background.png", "background");
+    background.setPaintRoutine(function (g) {
+        var a = [0, 0, 900, 600];
+        g.drawImage("background", a, 0, 0);
+    });
 
 
-const bigKnobLaf = Content.createLocalLookAndFeel();
-bigKnobLaf.loadImage("{PROJECT_FOLDER}bigPointer.png", "bigPointer");
-bigKnobLaf.registerFunction("drawRotarySlider", function(g, obj)
-{
-	var area = obj.area;
-	var start = 2.5;
-	var end = 2 * start * obj.valueNormalized - start;
-	g.rotate(end, [area[2]*0.5, area[3]*0.5]);
-	
-	g.drawImage("bigPointer", [72, 10, 15, 15], 0, 0);
-});
+    const bigKnobLaf = Content.createLocalLookAndFeel();
+    bigKnobLaf.loadImage("{PROJECT_FOLDER}bigPointer.png", "bigPointer");
+    bigKnobLaf.registerFunction("drawRotarySlider", function (g, obj) {
+        var area = obj.area;
+        var start = 2.5;
+        var end = 2 * start * obj.valueNormalized - start;
+        g.rotate(end, [area[2] * 0.5, area[3] * 0.5]);
 
-const var attackSelector = Content.getComponent("attackSelector");
-attackSelector.setLocalLookAndFeel(bigKnobLaf);
+        g.drawImage("bigPointer", [72, 10, 15, 15], 0, 0);
+    });
 
-const var sustainSelector = Content.getComponent("sustainSelector");
-sustainSelector.setLocalLookAndFeel(bigKnobLaf);
+    const var attackSelector = Content.getComponent("attackSelector");
+    attackSelector.setLocalLookAndFeel(bigKnobLaf);
 
-
-
-const lilKnobLaf = Content.createLocalLookAndFeel();
-lilKnobLaf.loadImage("{PROJECT_FOLDER}lilPointer.png", "lilPointer");
-lilKnobLaf.registerFunction("drawRotarySlider", function(g, obj)
-{
-	var area = obj.area;
-	var start = 2.5;
-	var end = 2 * start * obj.valueNormalized - start;
-	g.rotate(end, [area[2]*0.5, area[3]*0.5]);
-	
-	g.drawImage("lilPointer", [27, 6, 6, 6], 0, 0);
-});
-
-const var attackGain = Content.getComponent("attackGain");
-attackGain.setLocalLookAndFeel(lilKnobLaf);
-
-const var sustainGain = Content.getComponent("sustainGain");
-sustainGain.setLocalLookAndFeel(lilKnobLaf);
+    const var sustainSelector = Content.getComponent("sustainSelector");
+    sustainSelector.setLocalLookAndFeel(bigKnobLaf);
 
 
 
-inline function onAttackGainControl(component, value)
-{
-	Console.print("Callback chiamata con valore: " + value);
-	attackGainPanel.repaint();
-};
+    const lilKnobLaf = Content.createLocalLookAndFeel();
+    lilKnobLaf.loadImage("{PROJECT_FOLDER}lilPointer.png", "lilPointer");
+    lilKnobLaf.registerFunction("drawRotarySlider", function (g, obj) {
+        var area = obj.area;
+        var start = 2.5;
+        var end = 2 * start * obj.valueNormalized - start;
+        g.rotate(end, [area[2] * 0.5, area[3] * 0.5]);
 
-attackGain.setControlCallback(onAttackGainControl);
-const var attackGainPanel = Content.getComponent("attackGainPanel");
-attackGainPanel.setPaintRoutine(function(g) {
-	g.fillAll(Colours.transparentBlack);
+        g.drawImage("lilPointer", [27, 6, 6, 6], 0, 0);
+    });
+
+    const var attackGain = Content.getComponent("attackGain");
+    attackGain.setLocalLookAndFeel(lilKnobLaf);
+
+    const var sustainGain = Content.getComponent("sustainGain");
+    sustainGain.setLocalLookAndFeel(lilKnobLaf);
+
+const var attackGainEffect = Synth.getEffect("attackGain");
+inline function onAttackGainControl(component, value) {
+		attackGainEffect.setAttribute(0, value);
+        attackGainPanel.repaint();
+    };
+
+    attackGain.setControlCallback(onAttackGainControl);
+    const var attackGainPanel = Content.getComponent("attackGainPanel");
+    
+    attackGainPanel.setPaintRoutine(function (g) {
+        g.fillAll(Colours.transparentBlack);
 
 
-    var startOffset = 2.5;
-    var arcThickness = 5;
-    var margin = 2.5;
-    var normalizedValue = attackGain.getValueNormalized();
+        var startOffset = 2.5;
+        var arcThickness = 5;
+        var margin = 2.5;
+        var normalizedValue = attackGain.getValueNormalized();
 
-    function getPathArea(p, scale) {
-        var area = p.getBounds(scale);
-        area[0] += margin;
-        area[1] += margin;
-        return area;
-    }
+        function getPathArea(p, scale) {
+            var area = p.getBounds(scale);
+            area[0] += margin;
+            area[1] += margin;
+            return area;
+        }
 
-    var endOffset = Math.max(-startOffset + 2.0 * startOffset * normalizedValue, -startOffset + 0.1);
+        var endOffset = Math.max(-startOffset + 2.0 * startOffset * normalizedValue, -startOffset + 0.1);
 
 
-    var activeArc = Content.createPath();
-    activeArc.addArc([0, 0, 1, 1], -startOffset, endOffset);
-    g.setColour("0x7040798C");
-    var activeArea = getPathArea(activeArc, 85);
-    g.drawPath(activeArc, activeArea, arcThickness);
-});
+        var activeArc = Content.createPath();
+        activeArc.addArc([0, 0, 1, 1], -startOffset, endOffset);
+        g.setColour("0x7040798C");
+        var activeArea = getPathArea(activeArc, 85);
+        g.drawPath(activeArc, activeArea, arcThickness);
+    });
 
 
 
 
 
-inline function onSustainGainControl(component, value)
-{
-	Console.print("Callback chiamata con valore: " + value);
-	sustainGainPanel.repaint();
-};
-
-sustainGain.setControlCallback(onSustainGainControl);
-const var sustainGainPanel = Content.getComponent("sustainGainPanel");
-sustainGainPanel.setPaintRoutine(function(g) {
-	g.fillAll(Colours.transparentBlack);
-
-
-    var startOffset = 2.5;
-    var arcThickness = 5;
-    var margin = 2.5;
-    var normalizedValue = sustainGain.getValueNormalized();
-
-    function getPathArea(p, scale) {
-        var area = p.getBounds(scale);
-        area[0] += margin;
-        area[1] += margin;
-        return area;
-    }
-
-    var endOffset = Math.max(-startOffset + 2.0 * startOffset * normalizedValue, -startOffset + 0.1);
+const var sustainGainEffect = Synth.getEffect("sustainGain");
+inline function onSustainGainControl(component, value) {
+		sustainGainEffect.setAttribute(0, value);
+        sustainGainPanel.repaint();
+    };
+    
+    sustainGain.setControlCallback(onSustainGainControl);
+    const var sustainGainPanel = Content.getComponent("sustainGainPanel");
+    sustainGainPanel.setPaintRoutine(function (g) {
+        g.fillAll(Colours.transparentBlack);
 
 
-    var activeArc = Content.createPath();
-    activeArc.addArc([0, 0, 1, 1], -startOffset, endOffset);
-    g.setColour("0x7040798C");
-    var activeArea = getPathArea(activeArc, 85);
-    g.drawPath(activeArc, activeArea, arcThickness);
-});
+        var startOffset = 2.5;
+        var arcThickness = 5;
+        var margin = 2.5;
+        var normalizedValue = sustainGain.getValueNormalized();
+
+        function getPathArea(p, scale) {
+            var area = p.getBounds(scale);
+            area[0] += margin;
+            area[1] += margin;
+            return area;
+        }
+
+        var endOffset = Math.max(-startOffset + 2.0 * startOffset * normalizedValue, -startOffset + 0.1);
+
+
+        var activeArc = Content.createPath();
+        activeArc.addArc([0, 0, 1, 1], -startOffset, endOffset);
+        g.setColour("0x7040798C");
+        var activeArea = getPathArea(activeArc, 85);
+        g.drawPath(activeArc, activeArea, arcThickness);
+    });
+
 }
