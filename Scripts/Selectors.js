1a215f2d7cec950b534459878ebb93dabfc53020
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-09-07 15:12:38 +0200

    refactored Selector.js introducing a common function

--- Scripts/Selectors.js
+++ Scripts/Selectors.js
@@ -1,17 +1,24 @@
-namespace DSP {
-const var sampleMaps = Sampler.getSampleMapList();
+namespace Selectors {
+    const var sampleMaps = Sampler.getSampleMapList();
 
+    // Funzione generica per caricare i SampleMap, mantenuta come funzione normale
+    inline function loadSampleMapForSampler(sampler, value, offset) {
+        sampler.loadSampleMap(sampleMaps[value - 1 + offset]);
+    }
 
-const var attackSampler = Synth.getSampler("attackSampler");
-inline function onAttackSelectorControl(component, value) {
-	attackSampler.loadSampleMap(sampleMaps[value - 1]);
-};
-Content.getComponent("attackSelector").setControlCallback(onAttackSelectorControl);
+    // Inizializzazione del selettore dell'attacco
+    const var attackSampler = Synth.getSampler("attackSampler");
+    inline function onAttackSelectorControl(component, value) {
+        loadSampleMapForSampler(attackSampler, value, 0);
+    }
+    Content.getComponent("attackSelector").setControlCallback(onAttackSelectorControl);
 
-const var sustainSampler = Synth.getSampler("sustainSampler");
-inline function onSustainSelectorControl(component, value) {
-	sustainSampler.loadSampleMap(sampleMaps[value - 1 + 5]);
-};
-Content.getComponent("sustainSelector").setControlCallback(onSustainSelectorControl);
-} 
+    // Inizializzazione del selettore del sostegno
+    const var sustainSampler = Synth.getSampler("sustainSampler");
+    inline function onSustainSelectorControl(component, value) {
+        loadSampleMapForSampler(sustainSampler, value, 5);
+    }
+    Content.getComponent("sustainSelector").setControlCallback(onSustainSelectorControl);
+}
+
 
