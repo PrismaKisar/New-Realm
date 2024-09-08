45f0b8adccccae8d8ef624a5e485d2d13babea3e
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-09-08 17:32:45 +0200

    added background deleted on error

--- Scripts/GUI.js
+++ Scripts/GUI.js
@@ -1,3 +1,10 @@
 namespace GUI {
-
+	const var background = Content.getComponent("background");
+    background.loadImage("{PROJECT_FOLDER}background.png", "background");
+    background.setPaintRoutine(function (g) {
+        var a = [0, 0, 900, 600];
+        g.drawImage("background", a, 0, 0);
+    });
+	
+	
 }
